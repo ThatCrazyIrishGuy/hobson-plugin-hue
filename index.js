c1cd0bb5d4819e7b3943f119e8686b68f90d2519
Darren Britton <Darren Britton>
2017-05-08 00:21:54 +0100

    initial working version

---
+++ index.js
@@ -0,0 +1,70 @@
+exports = module.exports;
+const jsonfile = require('jsonfile');
+const path = require('path');
+const hue = require("node-hue-api"),
+    HueApi = hue.HueApi,
+    lightState = hue.lightState;
+
+const turnOn = lightState.create().on().brightness(100);
+const turnOff = lightState.create().off();
+
+function matchedGroup(api, values, defaultGroup){
+    const str = values.join(' ');
+    return api.getFullState().then((results)=>{
+      return Object.keys(results.groups).map(
+        key => results.groups[key]
+      )
+    }).then((groups)=>{
+      let roomIndex = 0;
+      groups.some((group, i)=>{
+        if(str.includes(group.name.toLowerCase())){
+          return roomIndex = i+1;
+        }
+      });
+      if(roomIndex > 0) return roomIndex;
+      return groups.findIndex(
+        group => group.name == defaultGroup
+      );
+    });
+}
+
+function createApi(config){
+  return new HueApi(config.hostname, config.username);
+}
+
+function getConfig(){
+  return jsonfile.readFileSync(path.resolve(__dirname, 'config.json'));
+}
+
+
+exports.lights = {
+    brightness: function(values, callback) {
+        const config = getConfig();
+        const api = createApi(config);
+        matchedGroup(api, values, config.defaultGroup)
+          .then((roomIndex) => {
+            const brightness = values[values.length - 1].replace('%', '');
+            setBrigtness = lightState.create().on().brightness(brightness);
+            api.setGroupLightState(roomIndex, setBrigtness);
+            return callback('setting light brightness to ' + brightness + ' percent');
+          });
+    },
+    on: function(values, callback) {
+      const config = getConfig();
+      const api = createApi(config);
+      matchedGroup(api, values, config.defaultGroup)
+        .then((roomIndex) => {
+          api.setGroupLightState(roomIndex, turnOn);
+          return callback('turning on the lights');
+        });
+    },
+    off: function(values, callback) {
+      const config = getConfig();
+      const api = createApi(config);
+      matchedGroup(api, values, config.defaultGroup)
+        .then((roomIndex) => {
+          api.setGroupLightState(roomIndex, turnOff);
+          return callback('turning off the lights');
+        });
+    }
+};
